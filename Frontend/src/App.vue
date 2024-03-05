9b9b9e8a509e08e66045fd3c7f8f5af6610f2101
AnatoleRch <anatoleroche.blq@gmail.com>
2024-03-05 09:14:00 +0100

    image sur la barre

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -3,16 +3,12 @@
   <!-- une barre de navigation -->
   <v-app-bar
   color="primary"
-  density="compact"
-  >
-  <!-- mettre l'icone dans le 'slot prepend' de 'v-app-bar' soit au début à gauche -->
-  <template v-slot:prepend>
-  <!-- un clic sur l'icone cache/affiche la zone de menu de gauche -->
-  <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-  </template>
-  <!-- le titre de la barre de navigation -->
-  <v-app-bar-title>Application recherche films</v-app-bar-title>
-  <!-- un simple bouton qui retourne à la page d'accueil route="/"-->
+  density="compact">
+  <v-img
+  :height="100"
+  aspect-ratio="1/1"
+  src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+  ></v-img>
   <v-btn to="/">Home</v-btn>
   <v-btn to="/Serre">S</v-btn>
   <v-btn to="/Pilotage">P</v-btn>
@@ -29,10 +25,17 @@
 </v-main>
 </v-app>
 </template>
+
+
 <script setup>
 </script>
+
+
 <style scoped>
 .page{
 text-align: center;
+}
+v-img{
+  float: left
 }
 </style>
